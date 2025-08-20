a10b78171845b6d6defae6928e6341fe371a2ec9
ding <dingjian@kodcloud.com>
2025-08-20 15:08:02 +0800

    避免旧插件覆盖

--- CONTROL/post-install.sh
+++ CONTROL/post-install.sh
@@ -10,7 +10,26 @@ case "$APKG_PKG_STATUS" in
 		# echo "KodBox_TEMP_DIR is $KodBox_TEMP_DIR" >> /var/log/kodbox.log
 		cp -af $KodBox_TEMP_DIR/setting_user.php $APKG_PKG_DIR/www/config/
 		cp -af $KodBox_TEMP_DIR/files $APKG_PKG_DIR/www/data/
-		cp -af $KodBox_TEMP_DIR/plugins $APKG_PKG_DIR/www/
+		src="$KodBox_TEMP_DIR/plugins/"
+		dst="$APKG_PKG_DIR/www/plugins/"
+
+		mkdir -p "$dst"
+
+		if command -v rsync >/dev/null 2>&1; then
+			# rsync 版本：目标已存在则跳过
+			rsync -a --ignore-existing "$src" "$dst"
+		else
+			# cp 版本：跳过已存在目录
+			for item in "$src"*; do
+				name=$(basename "$item")
+				if [ -d "$item" ] && [ -d "$dst/$name" ]; then
+					# echo "跳过目录: $dst/$name 已存在"
+					continue
+				fi
+				cp -af "$item" "$dst/"
+			done
+		fi
+
 		cp -af $KodBox_TEMP_DIR/install.lock $APKG_PKG_DIR/www/data/system/
 
 		# DB_USER=$(grep "DB_USER" $APKG_PKG_DIR/www/config/setting_user.php | cut -d "'" -f 4)
@@ -22,6 +41,8 @@ case "$APKG_PKG_STATUS" in
 			# mysql -u${DB_USER} -p${DB_PWD} ${DB_NAME} < $APKG_PKG_DIR/www/app/controller/install/data/update1.14/mysql.sql
 		fi
 
+		echo "`date +%Y-%m-%d-%H:%M:%S`-post-upgrade-executed, recover finished." >> /var/log/kodbox.log
+		
 		case "${DB_NAME}" in
                   *".php")
 					# echo "cp -af $KodBox_TEMP_DIR/${DB_NAME} $APKG_PKG_DIR/www/data/system/" >> /var/log/kodbox.log
